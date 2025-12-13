736bd645e407b54fa95fa1bd896903c1b622853b
Nobu-DD <n.s.mh256@gmail.com>
2025-12-14 05:48:12 +0900

    add:2〜18頭立ての単勝馬券をランダムで出力する機能実装

--- software/random_horse_racing_bets/js/bets.js
+++ software/random_horse_racing_bets/js/bets.js
@@ -1,1 +1,13 @@
+let numberOfRunners = document.getElementById('number_of_runners').value;
+let howToBuy = document.getElementById('how_to_buy').value;
+let outputButton = document.getElementById('output_button');
+let resultHowToBuy = document.getElementById('result_how_to_buy');
+let resultBettingNumber = document.getElementById('result_betting_number');
 
+function bettingSelectionsOutput() {
+  numberOfRunners = document.getElementById('number_of_runners').value;
+  resultHowToBuy.innerHTML = howToBuy;
+  resultBettingNumber.innerHTML = Math.floor(Math.random() * numberOfRunners) + 1;
+}
+
+outputButton.addEventListener("click", bettingSelectionsOutput);
