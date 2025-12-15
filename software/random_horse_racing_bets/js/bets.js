e3f68cfaa6a7db7b2a5c690e54fcbaf161ba88c0
Nobu-DD <n.s.mh256@gmail.com>
2025-12-16 01:43:24 +0900

    fix:出走頭数と買い方の定数定義をメソッド内に変更

--- software/random_horse_racing_bets/js/bets.js
+++ software/random_horse_racing_bets/js/bets.js
@@ -1,12 +1,11 @@
-let numberOfRunners = document.getElementById('number_of_runners').value;
-let howToBuy = document.getElementById('how_to_buy').value;
 let outputButton = document.getElementById('output_button');
 let resultHowToBuy = document.getElementById('result_how_to_buy');
 let resultBettingNumber = document.getElementById('result_betting_number');
 
 function bettingSelectionsOutput() {
-  numberOfRunners = document.getElementById('number_of_runners').value;
-  howToBuy = document.getElementById('how_to_buy').value;
+  const numberOfRunners = document.getElementById('number_of_runners').value;
+  const howToBuy = document.getElementById('how_to_buy').value;
+
   resultHowToBuy.innerHTML = howToBuy;
   resultBettingNumber.innerHTML = Math.floor(Math.random() * numberOfRunners) + 1;
 }
