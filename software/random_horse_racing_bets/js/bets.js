994b8c5281861b71c5e7098fe23eb4f99d1f2fe5
Nobu-DD <n.s.mh256@gmail.com>
2025-12-16 01:57:54 +0900

    fix:result画面の文字列代入をinnerHTMLからtextContentに変更

--- software/random_horse_racing_bets/js/bets.js
+++ software/random_horse_racing_bets/js/bets.js
@@ -1,13 +1,13 @@
-let outputButton = document.getElementById('output_button');
-let resultHowToBuy = document.getElementById('result_how_to_buy');
-let resultBettingNumber = document.getElementById('result_betting_number');
+const outputButton = document.getElementById('output_button');
+const resultHowToBuy = document.getElementById('result_how_to_buy');
+const resultBettingNumber = document.getElementById('result_betting_number');
 
 function bettingSelectionsOutput() {
   const numberOfRunners = document.getElementById('number_of_runners').value;
   const howToBuy = document.getElementById('how_to_buy').value;
 
-  resultHowToBuy.innerHTML = howToBuy;
-  resultBettingNumber.innerHTML = Math.floor(Math.random() * numberOfRunners) + 1;
+  resultHowToBuy.textContent = howToBuy;
+  resultBettingNumber.textContent = Math.floor(Math.random() * numberOfRunners) + 1;
 }
 
 outputButton.addEventListener("click", bettingSelectionsOutput);
