dcb8d7a2288a88d7d64dab783716fe32a9888c61
Nobu-DD <n.s.mh256@gmail.com>
2025-12-15 21:46:53 +0900

    add:買い方変更時にhowToBuy変数書き換え処理を追加

--- software/random_horse_racing_bets/js/bets.js
+++ software/random_horse_racing_bets/js/bets.js
@@ -6,6 +6,7 @@ let resultBettingNumber = document.getElementById('result_betting_number');
 
 function bettingSelectionsOutput() {
   numberOfRunners = document.getElementById('number_of_runners').value;
+  howToBuy = document.getElementById('how_to_buy').value;
   resultHowToBuy.innerHTML = howToBuy;
   resultBettingNumber.innerHTML = Math.floor(Math.random() * numberOfRunners) + 1;
 }
