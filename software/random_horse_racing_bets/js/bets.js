362a245616c583603b3acd9573349e670964d192
Nobu-DD <n.s.mh256@gmail.com>
2025-12-22 00:02:44 +0900

    add:ワイドを選択すると、2頭立てを削除する処理を追加

--- software/random_horse_racing_bets/js/bets.js
+++ software/random_horse_racing_bets/js/bets.js
@@ -1,19 +1,20 @@
 const outputButton = document.getElementById('output_button');
 const resultHowToBuy = document.getElementById('result_how_to_buy');
 const resultBettingNumber = document.getElementById('result_betting_number');
+const numberOfRunners = document.getElementById('number_of_runners');
+const selectedWide = document.getElementById('wide');
+const howToBuy = document.getElementById('how_to_buy');
 
 function bettingSelectionsOutput() {
-  const numberOfRunners = document.getElementById('number_of_runners').value;
-  const howToBuy = document.getElementById('how_to_buy').value;
 
-  resultHowToBuy.textContent = howToBuy;
-  if (howToBuy == "単勝" || howToBuy == "複勝") {
-    resultBettingNumber.textContent = Math.floor(Math.random() * numberOfRunners) + 1;
-  } else if (howToBuy == "ワイド") {
-    let countArray = Array.from({ length: numberOfRunners }, (v, i) => i+1);
-    let picks = [];
+  resultHowToBuy.textContent = howToBuy.value;
+  if (howToBuy.value == "単勝" || howToBuy.value == "複勝") {
+    resultBettingNumber.textContent = Math.floor(Math.random() * numberOfRunners.value) + 1;
+  } else if (howToBuy.value == "ワイド") {
+    const countArray = Array.from({ length: numberOfRunners.value }, (v, i) => i+1);
+    const picks = [];
     for (let i = 0; i < 3; i++) {
-      let selectedIndex = Math.floor(Math.random() * countArray.length);
+      const selectedIndex = Math.floor(Math.random() * countArray.length);
       picks.push(countArray[selectedIndex]);
       countArray.splice(selectedIndex, 1);
     }
@@ -26,4 +27,16 @@ function compareNumbers(a, b) {
   return a - b;
 }
 
+function changeRunners() {
+  const number2 = document.getElementById("number_2");
+
+  if (howToBuy.value == "ワイド") {
+    number2.style.display = "none";
+    numberOfRunners.value = numberOfRunners.value == "2" ? "3" : numberOfRunners.value;
+  } else {
+    number2.style.display = "block";
+  }
+}
+
 outputButton.addEventListener("click", bettingSelectionsOutput);
+howToBuy.addEventListener("change", changeRunners);
